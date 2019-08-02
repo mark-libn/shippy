b3353555b7a19e069cd371e3f6352e76501fa072
mark-libn <52531759+mark-libn@users.noreply.github.com>
2019-08-03 00:11:50 +0800

    Update extension.go

--- user-service/proto/user/extension.go
+++ user-service/proto/user/extension.go
@@ -6,9 +6,10 @@ import (
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, err := uuid.NewV4()
-	if err != nil {
-		return err
-	}
-	return scope.SetColumn("Id", uuid.String())
+// 	uuid, err := uuid.NewV4()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return scope.SetColumn("Id", uuid.String())
+	return scope.SetColumn("Id", uuid.MustNewV4().String())
 }
